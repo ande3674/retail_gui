89f85998b6e9332d4c1763b3bc818701f986db12
ce691 <email@email.com>
2018-10-21 16:22:12 -0500

    Simulate a cash register that can purchase items and update the database accordingly

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,5 @@ public class Main {
     public static void main(String[] args) {
         ItemDatabase db = new ItemDatabase();
         MainGUI mainGui = new MainGUI(db);
-
     }
 }
--- src/main/java/OpenCashierGUI.java
+++ src/main/java/OpenCashierGUI.java
@@ -2,6 +2,8 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.TreeMap;
+import java.util.Vector;
 
 public class OpenCashierGUI extends JDialog{
     private JTextField itemNumberTextField;
@@ -14,8 +16,14 @@ public class OpenCashierGUI extends JDialog{
 
     private ItemDatabase db;
 
+    // Variables to keep track of cashier info
+    private TreeMap<Integer, Integer> receipt; //Keys: id of item, Value: quantity left after purchase
+    private float totalPrice; // running total of the sale price
+
     protected OpenCashierGUI(ItemDatabase db){
         this.db = db;
+        this.receipt = new TreeMap();
+        this.totalPrice = (float)0.0;
         // Regular setup stuff
         setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
         setContentPane(mainPanel);
@@ -29,6 +37,56 @@ public class OpenCashierGUI extends JDialog{
     }
 
     private void addListeners() {
+
+        addButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    // Query the database for the item number and item info
+                    int id = Integer.parseInt(itemNumberTextField.getText());
+                    int quantity_desired = Integer.parseInt(quantityTextField.getText());
+                    Vector v = db.getItemById(id); // id, description, quantity, price
+                    String desc = (String)v.get(1);
+                    int quantity = (int)v.get(2);
+                    float price = (float)v.get(3);
+
+                    // Make sure there are enough of the items in inventory
+                    if (quantity < quantity_desired){//if inventory quantity is less than the desired quantity...
+                        // show error message
+                        // clear field so user can try again
+                        JOptionPane.showMessageDialog(OpenCashierGUI.this, "There are only "
+                                + quantity + " " + desc + "s in inventory, try again.");
+                        quantityTextField.setText("");
+                    }
+                    else { // else, there are enough so ...
+                        // Calculate total price and update the totalPrice variable
+                        totalPrice += quantity_desired * price;
+                        // Add information to the "receipt" data structure
+                        receipt.put(id, quantity-quantity_desired);
+                        // Update the text field(s)
+                        totalTextField.setText("$"+totalPrice);
+                    }
+
+                } catch (Exception ex){
+                    // Do something...
+                }
+            }
+        });
+        checkoutButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // Update the new quantity of the items in the inventory
+                for (int k : receipt.keySet()){
+                    db.updateItemQuantity(k, receipt.get(k));
+                }
+                // reset variables
+                receipt.clear();
+                totalPrice = (float)0.0;
+                // hide frame
+                setVisible(false);
+                // TODO display some sort of message
+            }
+        });
 
     }
 }
